17eafa39490378aa0eb55dd9a287301b6512b43f
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-06-08 18:16:31 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,78 @@
+const data = {
+  title1: "Quiet Time",
+  title2: "Study",
+  title3: "Go Jogging",
+  title4: "Eat Breakfast",
+  description1: "",
+  description2: "",
+  description3: "This is going to help to reach my goals and my life to the fullest",
+  description4: "",
+  date1: "05/02/2020",
+  date2: "01/02/2020",
+  date3: "tomorrow",
+  date4: "today",
+  time1: "08:12",
+  time2: "13:15",
+  time3: "12:36",
+  time4: "13:25",
+  completed1: false,
+  completed2: true,
+  completed3: false,
+  completed4: true,
+  priority1: "red",
+  priority2: "yellow",
+  priority3: "black",
+  priority4: "white",
+  tags1: ["Personal", "Work", "School"],
+  tags2: ["Personal", "School", "Diary Entry"],
+  tags3: ["Content Creation", "Personal"],
+  tags4: ["Personal"]
+}
+
+const {title1 , title2 , title3, title4 , description1 , description2 , decription3 , decription4 , date1 , date2 , date3 , date4 , time1 , time2 , time3 , time4 , completed1 , completed2 , completed3 ,completed4 , priority1 ,priority2 ,priority3 ,priority4 ,tags1 ,tags2 ,tags3 ,tags4} = data;
+
+console.log (title1 + ',' + description1 + ',' + date1 + ',' + time1 + ',' + completed1 + ',' + priority1 + ',' + tags1 )
+
+console.log (title2 + ',' + description2 + ',' + date2 + ',' + time2 + ',' + completed2 + ',' + priority2 + ',' + tags2 )
+
+console.log (title3 + ',' + description3 + ',' + date3 + ',' + time3 + ',' + completed3 + ',' + priority3 + ',' + tags3 )
+
+console.log (title4 + ',' + description4 + ',' + date4 + ',' + time4 + ',' + completed4 + ',' + priority4 + ',' + tags4 )
+
+//single object 
+
+console.log (title1 + ',' + title2 + ',' + title3 + ',' + title4)
+
+console.log(description1 + ',' + description2 + ',' +  decription3 + ',' + decription4 )
+
+console.log( date1 + ',' + date2 + ',' + date3 + ',' + date4 )
+
+console.log( time1 + ',' + time2 + ',' +  time3 + ',' +  time4)
+
+console.log(completed1 + ',' + completed2 + ',' + completed3 + ',' + completed4)
+
+console.log(priority1 + ',' + priority2 + ',' + priority3 + ',' + priority4)
+
+console.log(tags1 + ',' + tags2 + ',' + tags3 + ',' + tags4)
+
+
+
+
+
+
+/*const hero = {
+
+    Cname1: "Spiderman",
+    Cname2: "Ironman",
+    Cname3: "CaptainAmerica",
+    Cname4: "Blackwidow",
+    realName1: "Tom Holland",
+    realName2: "Robert Downey Jnr",
+    realName3: "Chris Evans",
+    realName4: "Scarlet Johnson",
+
+}
+
+const {Cname1 , realName1}  = hero;
+
+console.log (Cname1 + '-' + realName1)*/
